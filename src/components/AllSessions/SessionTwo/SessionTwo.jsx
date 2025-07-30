f30cee57ecf57d0b35e3527978e9bb51b65ecdef
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-07-30 23:05:28 +0400

    session two navigate to home page

--- src/components/AllSessions/SessionTwo/SessionTwo.jsx
+++ src/components/AllSessions/SessionTwo/SessionTwo.jsx
@@ -7,11 +7,24 @@ import {
   Hero,
   Testimonials,
 } from "./SessionTwoRoute.jsx";
+import { FaArrowLeftLong } from "react-icons/fa6";
+import { useNavigate } from "react-router-dom";
 import "./SessionTwo.css";
 
 const SessionTwo = () => {
+  const navigate = useNavigate();
+
   return (
     <div className="session_two_cont">
+      <div
+        className="back_home"
+        style={{ cursor: "pointer", position: "absolute", top: "0px", left: "20px" }}
+        onClick={() => navigate("/")}
+      >
+        <FaArrowLeftLong />
+        Back
+      </div>
+
       <Header />
       <Hero />
       <Feature />
