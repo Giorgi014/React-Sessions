04a9f2701bfdc26d53e0594419967d9731ab018d
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-07-31 00:32:43 +0400

    started session three home work

--- src/components/AllSessions/SessionThree/SessionThree.jsx
+++ src/components/AllSessions/SessionThree/SessionThree.jsx
@@ -1,9 +1,26 @@
-import './SessionThree.scss'
+import { useNavigate } from "react-router-dom";
+import { FaArrowLeftLong } from "react-icons/fa6";
+import "./SessionThree.css";
 
 const SessionThree = () => {
+  const navigate = useNavigate();
   return (
-    <div>SessionThree</div>
-  )
-}
+    <div className="session_three_cont">
+      <div
+        className="back_home"
+        style={{
+          cursor: "pointer",
+          position: "absolute",
+          top: "0px",
+          left: "20px",
+        }}
+        onClick={() => navigate("/")}
+      >
+        <FaArrowLeftLong />
+        Back
+      </div>
+    </div>
+  );
+};
 
-export default SessionThree
+export default SessionThree;
