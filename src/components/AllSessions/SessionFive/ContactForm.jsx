b23a9b0a340995a5a080a6aad9082038684d17a8
Giorgi014 <giorgi.gugunava99@gmail.com>
2025-08-03 23:50:15 +0400

    litle changes in stilization

--- src/components/AllSessions/SessionFive/ContactForm.jsx
+++ src/components/AllSessions/SessionFive/ContactForm.jsx
@@ -2,7 +2,7 @@ import { Input } from "./SessionFiveRoute.jsx";
 
 const ContactForm = () => {
   return (
-      <form className="bg-[#5C5C62] rounded-md p-5 space-y-6">
+      <form className="bg-[#5C5C62] rounded-md p-5 space-y-6 border bg-card text-card-foreground shadow-sm">
         <p className="text-2xl font-semibold">Send us a message</p>
       <label
         htmlFor="full name"
@@ -50,7 +50,7 @@ const ContactForm = () => {
         id="message"
         name="message"
         placeholder="Enter your message"
-        className="w-full px-4 py-3 form-input resize-none"
+        className="w-full px-4 py-3 form-input resize-none flex min-h-[80px] rounded-md border border-input bg-background text-sm ring-offset-background placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:cursor-not-allowed disabled:opacity-50"
       ></textarea>
     </form>
   );
--- src/components/AllSessions/SessionFive/SessionFive.jsx
+++ src/components/AllSessions/SessionFive/SessionFive.jsx
@@ -1,18 +1,22 @@
 import { ContactForm } from "./SessionFiveRoute.jsx";
+import { RiResetLeftFill } from "react-icons/ri";
 
 const SessionFive = () => {
   return (
-    <div className="w-screen bg-[#15151A]">
+    <div className="min-h-screen p-4 md:p-8 bg-[#15151A]">
       <div className="flex justify-between items-center">
-        <div className="animate-fade-in">
-          <h1 class="font-bold bg-gradient-to-r from-indigo-950 to-purple-400 bg-clip-text text-transparent text-4xl md:text-6xl">
+        <div className="max-w-7xl mx-auto mb-8">
+          <h1 class="font-bold bg-gradient-to-r from-indigo-950 to-purple-300 bg-clip-text text-transparent text-4xl md:text-6xl">
             Contact Us
           </h1>
           <p className="text-muted-foreground mt-2 text-lg text-slate-500">
             Get in touch with our team. We'd love to hear from you!
           </p>
         </div>
-        <div>reset</div>
+        <div className="flex gap-2 items-center justify-around cursor-pointer text-white w-25 h-12 bg-gray-900 hover:bg-gray-500 border-zinc-300 border">
+          <RiResetLeftFill />
+          <p>Reset</p>
+        </div>
       </div>
       <div className="flex justify-between items-center">
         <ContactForm />
